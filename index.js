268a728a93cb65121073a88d17797c311c29e9b9
nvlannasik <annasikn@yahoo.com>
2022-08-11 13:04:21 +0700

    finishing code & add readme

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-"use strict";
 require("dotenv").config();
 const cron = require("node-cron");
 const TelegramBot = require("node-telegram-bot-api");
 const { Client } = require("@elastic/elasticsearch");
+
+///==================
+/// ELASTICSEARCH AUTH LOGIN TO SERVER
+///=================
+
 const client = new Client({
   node: process.env.ELASTIC_URL,
   auth: {
@@ -10,16 +14,26 @@ const client = new Client({
     password: process.env.ELASTIC_PASSWORD,
   },
 });
+
+///==================
+/// TELEGRAM BOT AUTH
+///==================
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token);
 const chatId = process.env.TELEGRAM_CHAT_ID;
 
-var itung = 0;
+///=================
+/// CRON JOB SETIAP 1 MENIT
+///=================
 
 const task = cron.schedule("* * * * *", () => {
   run().catch(console.log);
 });
 
+var itung = 0;
+///==================
+/// ELASTICSEARCH COUNTING JUMLAH DATA DI INDEX
+///==================
 async function run() {
   const count = await client.count({
     index: process.env.ELASTIC_INDEX,
@@ -30,6 +44,9 @@ async function run() {
     },
   });
 
+  ///==================
+  /// ELASTICSEARCH CARI DATA TERBARU DAN KIRIM KE TELEGRAM
+  ///==================
   const getQuery = await client.search({
     index: process.env.ELASTIC_INDEX,
     body: {
@@ -40,26 +57,34 @@ async function run() {
     size: count.count,
     sort: [{ timestamp: {} }],
   });
-  // itung = count.count;
+
   if (itung != count.count || itung > count.count) {
     bot.sendMessage(
       chatId,
-      `[WARNING] CPU USAGE DARI SERVICE ${getQuery.hits.hits
+      `CPU USAGE DARI SERVICE ${getQuery.hits.hits
         .slice(-1)
         .map((item) => item._source.alert_id)} MENCAPAI ${getQuery.hits.hits
         .slice(-1)
-        .map((item) => item._source.value)}%`
+        .map((item) => item._source.value)}`
     );
-    console.log("kirim pesan");
+    console.log("Ada data baru dan kirim pesan");
   } else {
-    console.log("nothing");
+    console.log("Ga ada Update yang harus dikirim");
   }
   itung = count.count;
+
+  //=============
+  //BUAT DEBUG
+  //=============
   // const lastItem = getQuery.hits.hits
   //   .map((item) => item._source.value)
   //   .slice(-1);
+
   // console.log(lastItem);
 }
 
 task.start();
-run().catch(console.log);
+//=============
+//BUAT DEBUG
+//=============
+// run().catch(console.log);
